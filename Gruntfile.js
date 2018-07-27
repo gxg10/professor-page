1f584d75dc7b03737ac61de608e65fc268358712
George Brande <george.brande@gmail.com>
2018-07-27 11:54:06 +0300

    grunt part 1

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+	require('time-grunt')(grunt);
+
+	require('jit-grunt')(grunt);
+
+	grunt.initConfig({
+		sass: {
+			dist: {
+				files: {
+					'css/styles.css': 'css/styles.scss'
+				}
+			}
+		},
+		watch: {
+			files: 'css/*.css', 
+			tasks: ['sass']
+		},
+		browserSync: {
+			dev: {
+				bsFiles: {
+					src: [
+						'css/*.css',
+						'*.html',
+						'js/*.js'
+					]
+				}
+			}
+		},
+		options: {
+			watchTaks: true,
+			server: {
+				baseDir: './'
+			}
+		}
+
+	});
+
+	grunt.registerTask('css', ['sass']);
+	grunt.registerTask('default', ['browserSync', 'watch']);
+};
